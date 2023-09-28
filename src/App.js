7ca7a6776d1a1ead4b226ae75daf4d5235332b60
jhon quiceno <jonasquiceno@gmail.com>
2023-09-28 16:58:11 +0000

    Creaccion componente de lista de productos

--- src/App.js
+++ src/App.js
@@ -1,65 +1,33 @@
-import { useState, useEffect} from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css'
-import Producto from './componentes/producto';
-import Boton from './componentes/boton';
-import Usuario from './componentes/usuario';
-import ProductoDetallado from './componentes/productoDetallado';
-
-
-
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Usuario from "./componentes/usuario";
+import ProductList from "./componentes/productList";
+import ProductoDetallado from "./componentes/productoDetallado";
 
 function App() {
-
-  const [producto, setProducto] = useState([]);
-  const [mostrarTodo, setMostrarTodo] = useState(false);
   const [usuario, setUsuario] = useState(null);
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(producto=>setProducto(producto))
+    fetch("https://fakestoreapi.com/users/5")
+      .then((res) => res.json())
+      .then((usuario) => setUsuario(usuario));
   }, []);
-
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/users/5')
-            .then(res=>res.json())
-            .then(usuario=>setUsuario(usuario))
-  }, []);
-
-  const mostrarProductos = mostrarTodo? producto : producto.slice(0,4);
-
-  const funcionClic = () => {
-    setMostrarTodo(!mostrarTodo);
-  }
 
   return (
     <div className="App">
-      <header className='header'>
-      <h1 className='titulo'>Online Shop</h1>
-      {usuario && (
-        <Usuario
-          nombre={usuario.name.firstname}
-          apellido={usuario.name.lastname}/>
-      )}  
-      </header>
-      <div className='contenedor-principal'>
-        {mostrarProductos.map((producto,id)=>(
-          <Producto
-          id={id+1}
-          titulo={producto.title}
-          imagen={producto.image}
-          precio={producto.price}
+      <header className="header">
+        <h1 className="titulo">Online Shop</h1>
+        {usuario && (
+          <Usuario
+            nombre={usuario.name.firstname}
+            apellido={usuario.name.lastname}
           />
-        ))}
-        <Boton className='boton'
-          texto={mostrarTodo ? 'ver menos' : 'ver mas'}
-          funcionClic={funcionClic}
-        />
-      </div>
+        )}
+      </header>
       <Routes>
-          <Route path="/" element={<Producto />} />
-          <Route path="/producto/:id" element={<ProductoDetallado />} />
+        <Route path="/" element={<ProductList />} />
+        <Route path="/producto/:id" element={<ProductoDetallado />} />
       </Routes>
     </div>
   );
---
+++ src/componentes/productList.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+import Producto from "./producto";
+import Boton from "./boton";
+
+// se debe poner por fuera la lista recordar que el react-router
+// renderiza o no un componente segun la url
+
+const ProductList = () => {
+  const [producto, setProducto] = useState([]);
+  const [mostrarTodo, setMostrarTodo] = useState(false);
+
+  const mostrarProductos = mostrarTodo ? producto : producto.slice(0, 4);
+
+  const funcionClic = () => {
+    setMostrarTodo(!mostrarTodo);
+  };
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((producto) => setProducto(producto));
+  }, []);
+  return (
+    <div className="contenedor-principal">
+      {mostrarProductos.map((producto, id) => (
+        <Producto
+          id={id + 1}
+          titulo={producto.title}
+          imagen={producto.image}
+          precio={producto.price}
+        />
+      ))}
+      <Boton
+        className="boton"
+        texto={mostrarTodo ? "ver menos" : "ver mas"}
+        funcionClic={funcionClic}
+      />
+    </div>
+  );
+};
+
+export default ProductList;
